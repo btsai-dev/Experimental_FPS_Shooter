2a0658b78095214f96dc70b2dc001a983eb870f8
godonan <godonanabt@gmail.com>
2020-01-28 02:00:18 -0600

    Completed Lighting, hit particles for test weapon.

--- FPS Alpha Experimental/Assets/Scripts/Damage Testing/ObjectHealth.cs
+++ FPS Alpha Experimental/Assets/Scripts/Damage Testing/ObjectHealth.cs
@@ -4,15 +4,99 @@ using UnityEngine;
 
 public class ObjectHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+    public int currentHealth;                   // The current health the enemy has.
+    public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+    public AudioClip deathClip;                 // The sound to play when the enemy dies.
+
+    AudioSource enemyAudio;                     // Reference to the audio source.
+    ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
+    BoxCollider boxCollider;            // Reference to the capsule collider.
+
+    bool isDead;                                // Whether the enemy is dead.
+    bool isSinking;                             // Whether the enemy has started sinking through the floor.
+
+
+    void Awake()
     {
-        
+        // Setting up the references.
+        enemyAudio = GetComponent<AudioSource>();
+        hitParticles = GetComponentInChildren<ParticleSystem>();
+        boxCollider = GetComponent<BoxCollider>();
+
+        // Setting the current health when the enemy first spawns.
+        currentHealth = startingHealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // If the enemy should be sinking...
+        if (isSinking)
+        {
+            Debug.Log("He should be sinking...");
+            // ... move the enemy down by the sinkSpeed per second.
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        // If the enemy is dead...
+        if (isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+        // Play the hurt sound effect.
+        enemyAudio.Play();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+        // Set the position of the particle system to where the hit was sustained.
+        hitParticles.transform.position = hitPoint;
+
+        // And play the particles.
+        hitParticles.Play();
+
+        // If the current health is less than or equal to zero...
+        if (currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        Debug.Log("ENEMY DEAD!");
+        // The enemy is dead.
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it.
+        boxCollider.isTrigger = true;
+
+        // Tell the animator that the enemy is dead.
+        // anim.SetTrigger("Dead");
+
+
+        // Temp force sink
+        StartSinking();
+
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+    }
+
+
+    public void StartSinking()
+    {
         
+        // The enemy should now sink.
+        isSinking = true;
+
+        // After 2 seconds destory the enemy.
+        Destroy(gameObject, 2f);
     }
 }
